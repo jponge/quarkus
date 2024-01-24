995f63092354e3f3a7d5fdfac7a0536d506c7cf3
Julien Ponge <jponge@redhat.com>
2024-01-24 22:58:22 +0100

    SSE example + README

--- integration-tests/mutiny-native-jctools/src/main/java/io/quarkus/it/mutiny/nativejctools/MyResource.java
+++ integration-tests/mutiny-native-jctools/src/main/java/io/quarkus/it/mutiny/nativejctools/MyResource.java
@@ -4,13 +4,21 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.reactive.RestStreamElementType;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.helpers.queues.Queues;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import io.smallrye.mutiny.subscription.MultiEmitter;
 
 @Path("/tests")
 public class MyResource {
@@ -49,5 +57,29 @@ public class MyResource {
                 .select().first(10)
                 .collect().asList().await().atMost(Duration.ofSeconds(5));
         return String.join("", ticks);
+    }
+
+    @GET
+    @RestStreamElementType(MediaType.TEXT_PLAIN)
+    @Path("emitter")
+    public Multi<String> emitter() {
+        AtomicInteger counter = new AtomicInteger();
+        return Multi.createFrom().emitter(emitter -> {
+            ScheduledExecutorService scheduler = Infrastructure.getDefaultWorkerPool();
+            scheduler.schedule(() -> emitAndSchedule(counter, emitter), 100, TimeUnit.MILLISECONDS);
+        });
+    }
+
+    private void emitAndSchedule(AtomicInteger counter, MultiEmitter<? super String> emitter) {
+        ScheduledExecutorService scheduler = Infrastructure.getDefaultWorkerPool();
+        int n = counter.getAndIncrement();
+        if (n < 5) {
+            for (int i = 0; i < n * 10; i++) {
+                emitter.emit(String.valueOf(i));
+            }
+            scheduler.schedule(() -> emitAndSchedule(counter, emitter), 125, TimeUnit.MILLISECONDS);
+        } else {
+            emitter.complete();
+        }
     }
 }
