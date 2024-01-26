f87865fa2694e2f030688d8394e8d8d685d553c9
Julien Ponge <jponge@redhat.com>
2024-01-26 11:43:06 +0100

    Style

--- extensions/mutiny/deployment/src/main/java/io/quarkus/mutiny/deployment/MutinyProcessor.java
+++ extensions/mutiny/deployment/src/main/java/io/quarkus/mutiny/deployment/MutinyProcessor.java
@@ -1,5 +1,10 @@
 package io.quarkus.mutiny.deployment;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.jboss.threads.ContextHandler;
+
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
@@ -8,19 +13,15 @@ import io.quarkus.deployment.builditem.ExecutorBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.UnsafeAccessedFieldBuildItem;
 import io.quarkus.mutiny.runtime.MutinyInfrastructure;
-import org.jboss.threads.ContextHandler;
-
-import java.util.List;
-import java.util.Optional;
 
 public class MutinyProcessor {
 
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
     public void runtimeInit(ExecutorBuildItem executorBuildItem,
-                            MutinyInfrastructure recorder,
-                            ShutdownContextBuildItem shutdownContext,
-                            Optional<ContextHandlerBuildItem> contextHandler) {
+            MutinyInfrastructure recorder,
+            ShutdownContextBuildItem shutdownContext,
+            Optional<ContextHandlerBuildItem> contextHandler) {
         ContextHandler<Object> handler = contextHandler.map(ContextHandlerBuildItem::contextHandler).orElse(null);
         recorder.configureMutinyInfrastructure(executorBuildItem.getExecutorProxy(), shutdownContext, handler);
     }
@@ -38,7 +39,6 @@ public class MutinyProcessor {
         return List.of(
                 new UnsafeAccessedFieldBuildItem(
                         "org.jctools.util.UnsafeRefArrayAccess",
-                        "REF_ELEMENT_SHIFT")
-        );
+                        "REF_ELEMENT_SHIFT"));
     }
 }
